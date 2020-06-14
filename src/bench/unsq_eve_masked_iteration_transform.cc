f1eff4aea8d4e9379b9ce1cdd452c28d185d9990
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2020-06-14 20:51:08 +0100

    two iteration patterns transform benchmarks

--- src/bench/unsq_eve_masked_iteration_transform.cc
+++ src/bench/unsq_eve_masked_iteration_transform.cc
@@ -22,9 +22,9 @@
 namespace {
 
 template <std::size_t width, std::size_t unroll>
-struct unsq_eve_transform {
+struct unsq_eve_transform_masked {
   std::string name() const {
-    return "unsq_eve_transform<" + std::to_string(width) + ", " +
+    return "unsq_eve_transform_masked<" + std::to_string(width) + ", " +
            std::to_string(unroll) + '>';
   }
 
@@ -33,7 +33,7 @@ struct unsq_eve_transform {
     using traits =
         unsq_eve::algorithm_traits<unsq_eve::value_type<I>, width, unroll>;
 
-    unsq_eve::transform<traits>(f, l, [](auto x) { return x + x; });
+    unsq_eve::transform_masked<traits>(f, l, [](auto x) { return x + x; });
   }
 };
 
@@ -41,14 +41,14 @@ struct unsq_eve_transform {
 
 int main(int argc, char** argv) {
   using char_bench =
-      bench::inplace_transform_bench<char, unsq_eve_transform<128, 1>,
-                                     unsq_eve_transform<256, 1>,
-                                     unsq_eve_transform<256, 4>>;
+      bench::inplace_transform_bench<char, unsq_eve_transform_masked<128, 1>,
+                                     unsq_eve_transform_masked<256, 1>,
+                                     unsq_eve_transform_masked<256, 4>>;
   using short_bench =
-      bench::inplace_transform_bench<short, unsq_eve_transform<256, 4>>;
+      bench::inplace_transform_bench<short, unsq_eve_transform_masked<256, 4>>;
 
   using int_bench =
-      bench::inplace_transform_bench<int, unsq_eve_transform<256, 4>>;
+      bench::inplace_transform_bench<int, unsq_eve_transform_masked<256, 4>>;
 
   bench::bench_main<char_bench, short_bench, int_bench>(argc, argv);
 }
