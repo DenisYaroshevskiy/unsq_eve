28ca6065992e659d169c82a596ef5a02c4262299
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2021-09-26 22:30:26 +0100

    remove ints sse2

--- disassemble/disassemble.cc
+++ disassemble/disassemble.cc
@@ -1,10 +1,10 @@
 #include <eve/algo/remove.hpp>
 #include <eve/algo/as_range.hpp>
 
-#include <eve/algo/inclusive_scan.hpp>
+#include <vector>
 
 using T = int;
 
-void inclusive_scan(T* f, T* l) {
-  eve::algo::inclusive_scan_inplace(eve::algo::as_range(f, l), eve::zero);
+void remove(std::vector<T>& v) {
+  eve::algo::remove(v, 0);
 }
