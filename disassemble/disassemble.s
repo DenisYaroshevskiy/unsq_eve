8bc4a6ca6c6785560912579bec097cbf6808f15b
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2022-01-28 01:06:52 +0000

    fix compress 8

--- disassemble/disassemble.s
+++ disassemble/disassemble.s
@@ -19,118 +19,352 @@
 _Z6removeRNSt3__16vectorIiNS_9allocatorIiEEEE: # @_Z6removeRNSt3__16vectorIiNS_9allocatorIiEEEE
 	.cfi_startproc
 # %bb.0:
-	push	rbx
-	.cfi_def_cfa_offset 16
-	.cfi_offset rbx, -16
-	mov	rax, qword ptr [rdi]
+	mov	rdx, qword ptr [rdi]
 	mov	r8, qword ptr [rdi + 8]
-	cmp	rax, r8
+	cmp	rdx, r8
 	je	.LBB0_9
 # %bb.1:
-	mov	rdx, rax
-	and	rdx, -32
-	mov	rbx, r8
-	and	rbx, -32
-	shr	eax, 2
-	and	eax, 7
-	movabs	r10, 72340172838076673
-	movabs	r9, 506097522914230528
-	cmp	rdx, rbx
+	mov	rdi, rdx
+	and	rdi, -32
+	mov	r10, r8
+	and	r10, -32
+	shr	edx, 2
+	and	edx, 7
+	cmp	rdi, r10
 	je	.LBB0_2
 # %bb.3:
-	vmovdqa	ymm1, ymmword ptr [rdx]
+	vmovdqa	ymm1, ymmword ptr [rdi]
 	vpxor	xmm0, xmm0, xmm0
 	vpcmpeqd	ymm2, ymm1, ymm0
-	vmovmskps	edi, ymm2
-	xor	edi, 255
-	shrx	edi, edi, eax
-	shlx	r11d, edi, eax
-	pdep	rdi, r11, r10
-	mov	rcx, rdi
-	shl	rcx, 8
-	sub	rcx, rdi
-	pext	rcx, r9, rcx
-	vmovq	xmm2, rcx
-	vpmovzxbd	ymm2, xmm2              # ymm2 = xmm2[0],zero,zero,zero,xmm2[1],zero,zero,zero,xmm2[2],zero,zero,zero,xmm2[3],zero,zero,zero,xmm2[4],zero,zero,zero,xmm2[5],zero,zero,zero,xmm2[6],zero,zero,zero,xmm2[7],zero,zero,zero
-	popcnt	ecx, r11d
-	vmovd	xmm3, ecx
+	vmovd	xmm3, edx
 	vpbroadcastd	ymm3, xmm3
-	vpcmpgtd	ymm3, ymm3, ymmword ptr [rip + .LCPI0_0]
+	vmovdqa	ymm4, ymmword ptr [rip + .LCPI0_0] # ymm4 = [0,1,2,3,4,5,6,7]
+	vpcmpgtd	ymm3, ymm3, ymm4
+	vpor	ymm2, ymm2, ymm3
+	vpshufd	ymm3, ymm1, 57                  # ymm3 = ymm1[1,2,3,0,5,6,7,4]
+	vpblendvb	ymm1, ymm1, ymm3, ymm2
+	vpmovmskb	esi, ymm2
+	not	esi
+	mov	eax, esi
+	and	eax, 2062097
+	popcnt	r9d, eax
+	popcnt	eax, esi
+	shr	esi, 17
+	and	esi, 13
+	add	esi, r9d
+	shl	rsi, 5
+	vmovdqa	ymm2, ymmword ptr [rsi + _ZN3eve6detail27pattern_8_elements_dwords_vIjEE]
 	vpermd	ymm1, ymm2, ymm1
-	vpmaskmovd	ymmword ptr [rdx + 4*rax], ymm3, ymm1
-	lea	rax, [rdx + 4*rax]
-	lea	rdi, [rax + 4*rcx]
-	add	rdx, 32
-	cmp	rdx, rbx
+	vmovd	xmm2, eax
+	and	eax, -4
+	vpbroadcastd	ymm2, xmm2
+	vpsrld	ymm2, ymm2, 2
+	vpcmpgtd	ymm2, ymm2, ymm4
+	vpmaskmovd	ymmword ptr [rdi + 4*rdx], ymm2, ymm1
+	lea	rsi, [rdi + 4*rdx]
+	add	rsi, rax
+	add	rdi, 32
+	cmp	rdi, r10
 	je	.LBB0_6
 	.p2align	4, 0x90
 .LBB0_4:                                # =>This Inner Loop Header: Depth=1
-	vmovdqa	ymm1, ymmword ptr [rdx]
+	vmovdqa	ymm1, ymmword ptr [rdi]
 	vpcmpeqd	ymm2, ymm1, ymm0
-	vmovmskps	eax, ymm2
-	xor	eax, 255
-	pdep	rsi, rax, r10
-	mov	rcx, rsi
-	shl	rcx, 8
-	sub	rcx, rsi
-	pext	rcx, r9, rcx
-	vmovq	xmm2, rcx
-	vpmovzxbd	ymm2, xmm2              # ymm2 = xmm2[0],zero,zero,zero,xmm2[1],zero,zero,zero,xmm2[2],zero,zero,zero,xmm2[3],zero,zero,zero,xmm2[4],zero,zero,zero,xmm2[5],zero,zero,zero,xmm2[6],zero,zero,zero,xmm2[7],zero,zero,zero
+	vpshufd	ymm3, ymm1, 57                  # ymm3 = ymm1[1,2,3,0,5,6,7,4]
+	vpblendvb	ymm1, ymm1, ymm3, ymm2
+	vpmovmskb	eax, ymm2
+	not	eax
+	mov	edx, eax
+	and	edx, 2062097
+	popcnt	edx, edx
+	popcnt	ecx, eax
+	shr	eax, 17
+	and	eax, 13
+	add	eax, edx
+	and	ecx, -4
+	shl	rax, 5
+	vmovdqa	ymm2, ymmword ptr [rax + _ZN3eve6detail27pattern_8_elements_dwords_vIjEE]
 	vpermd	ymm1, ymm2, ymm1
-	popcnt	rax, rax
-	vmovdqu	ymmword ptr [rdi], ymm1
-	lea	rdi, [rdi + 4*rax]
-	add	rdx, 32
-	cmp	rdx, rbx
+	vmovdqu	ymmword ptr [rsi], ymm1
+	add	rsi, rcx
+	add	rdi, 32
+	cmp	rdi, r10
 	jne	.LBB0_4
 .LBB0_6:
-	cmp	r8, rbx
+	cmp	r8, r10
 	je	.LBB0_9
 # %bb.7:
-	xor	eax, eax
+	xor	edx, edx
 	jmp	.LBB0_8
 .LBB0_2:
-	mov	rdi, rdx
+	mov	rsi, rdi
 .LBB0_8:
-	mov	ecx, ebx
-	sub	ecx, r8d
-	add	ecx, 32
-	shr	ecx, 2
-	vmovdqa	ymm0, ymmword ptr [rbx]
-	vpxor	xmm1, xmm1, xmm1
-	vpcmpeqd	ymm1, ymm0, ymm1
-	vmovmskps	edx, ymm1
-	xor	edx, 255
-	mov	sil, 8
-	sub	sil, cl
-	mov	rcx, -1
-	shlx	rcx, rcx, rsi
-	not	ecx
-	shrx	ecx, ecx, eax
-	shlx	ecx, ecx, eax
-	and	ecx, edx
-	pdep	rdx, rcx, r10
-	mov	rsi, rdx
-	shl	rsi, 8
-	sub	rsi, rdx
-	pext	rdx, r9, rsi
-	vmovq	xmm1, rdx
-	vpmovzxbd	ymm1, xmm1              # ymm1 = xmm1[0],zero,zero,zero,xmm1[1],zero,zero,zero,xmm1[2],zero,zero,zero,xmm1[3],zero,zero,zero,xmm1[4],zero,zero,zero,xmm1[5],zero,zero,zero,xmm1[6],zero,zero,zero,xmm1[7],zero,zero,zero
+	mov	rax, r10
+	sub	rax, r8
+	add	rax, 32
+	shr	rax, 2
+	vmovdqa	ymm1, ymmword ptr [r10]
+	vpxor	xmm0, xmm0, xmm0
+	vpcmpeqd	ymm2, ymm1, ymm0
+	vmovd	xmm0, edx
+	vpbroadcastd	ymm3, xmm0
+	vmovdqa	ymm0, ymmword ptr [rip + .LCPI0_0] # ymm0 = [0,1,2,3,4,5,6,7]
+	vpcmpgtd	ymm3, ymm3, ymm0
+	vpor	ymm2, ymm3, ymm2
+	mov	ecx, 8
+	sub	ecx, eax
+	vmovd	xmm3, ecx
+	vpbroadcastd	ymm3, xmm3
+	vpcmpgtd	ymm3, ymm3, ymm0
+	vpandn	ymm2, ymm2, ymm3
+	vpshufd	ymm3, ymm1, 57                  # ymm3 = ymm1[1,2,3,0,5,6,7,4]
+	vpblendvb	ymm1, ymm3, ymm1, ymm2
+	vpmovmskb	eax, ymm2
+	mov	ecx, eax
+	and	ecx, 2062097
 	popcnt	ecx, ecx
-	vmovd	xmm2, ecx
+	popcnt	edi, eax
+	shr	eax, 17
+	and	eax, 13
+	add	eax, ecx
+	shl	rax, 5
+	vmovdqa	ymm2, ymmword ptr [rax + _ZN3eve6detail27pattern_8_elements_dwords_vIjEE]
+	vpermd	ymm1, ymm2, ymm1
+	shr	edi, 2
+	vmovd	xmm2, edi
 	vpbroadcastd	ymm2, xmm2
-	vpcmpgtd	ymm2, ymm2, ymmword ptr [rip + .LCPI0_0]
-	vpermd	ymm0, ymm1, ymm0
-	vpmaskmovd	ymmword ptr [rdi + 4*rax], ymm2, ymm0
+	vpcmpgtd	ymm0, ymm2, ymm0
+	vpmaskmovd	ymmword ptr [rsi + 4*rdx], ymm0, ymm1
 .LBB0_9:
-	pop	rbx
-	.cfi_def_cfa_offset 8
 	vzeroupper
 	ret
 .Lfunc_end0:
 	.size	_Z6removeRNSt3__16vectorIiNS_9allocatorIiEEEE, .Lfunc_end0-_Z6removeRNSt3__16vectorIiNS_9allocatorIiEEEE
 	.cfi_endproc
                                         # -- End function
+	.type	_ZN3eve6detail27pattern_8_elements_dwords_vIjEE,@object # @_ZN3eve6detail27pattern_8_elements_dwords_vIjEE
+	.section	.rodata._ZN3eve6detail27pattern_8_elements_dwords_vIjEE,"aG",@progbits,_ZN3eve6detail27pattern_8_elements_dwords_vIjEE,comdat
+	.weak	_ZN3eve6detail27pattern_8_elements_dwords_vIjEE
+	.p2align	5
+_ZN3eve6detail27pattern_8_elements_dwords_vIjEE:
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	2                               # 0x2
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	2                               # 0x2
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	4                               # 0x4
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	2                               # 0x2
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.long	0                               # 0x0
+	.long	0                               # 0x0
+	.long	1                               # 0x1
+	.long	2                               # 0x2
+	.long	3                               # 0x3
+	.long	4                               # 0x4
+	.long	5                               # 0x5
+	.long	6                               # 0x6
+	.long	7                               # 0x7
+	.size	_ZN3eve6detail27pattern_8_elements_dwords_vIjEE, 864
+
 	.section	".linker-options","e",@llvm_linker_options
 	.ident	"clang version 14.0.0 (https://github.com/llvm/llvm-project.git 52fddcdd9c90a550d7a50cbc2013be3314f91d08)"
 	.section	".note.GNU-stack","",@progbits
--- disassemble/disassemble.sh
+++ disassemble/disassemble.sh
@@ -1,1 +1,1 @@
-clang++ -std=c++20 -mavx2 -mbmi2 -DEVE_USE_BMI_ON_AVX2 --stdlib=libc++ -O3 -DEVE_DISABLE_ASSERTS -I  ../../eve/include -I ../src -S -mllvm --x86-asm-syntax=intel disassemble.cc
+clang++ -std=c++20 -mavx2 -mbmi2 --stdlib=libc++ -O3 -DEVE_DISABLE_ASSERTS -I  ../../eve/include -I ../src -S -mllvm --x86-asm-syntax=intel disassemble.cc
